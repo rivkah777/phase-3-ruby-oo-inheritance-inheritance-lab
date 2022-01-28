4cd72ad1ff8daa8c30ac66c2d35cc4957e8dee99
Rebekah <rbk1008@Gmail.com>
2022-01-28 10:28:49 -0700

    commit tree 413d4cbb03461c80574225d2744b8e581d15cebb

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
 
+        attr_accessor :knowledge
+
+    # Individual students should initialize with an instance variable,
+    #  @knowledge, that points to an empty array.
+    def initialize 
+        @knowledge = []
+    end 
+
+
+
+    # Define a method, #knowledge, that returns that student's knowledge array.
+    def learn (knowledge)
+        @knowledge << knowledge
+    end
 end
